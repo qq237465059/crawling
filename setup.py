15208b7729104d065139a5f0ff4b51153fcccd07
胡一 <yhu@ittx.com.cn>
2017-12-21 13:01:30 +0800

    部署

--- setup.py
+++ setup.py
@@ -8,6 +8,5 @@ setup(
     description='爬虫demo',
     py_modules=['com.crawling.dao.runSpider'],
     packages=['com', 'com.crawling', 'com.crawling.config', 'com.crawling.database', 'com.crawling.domain', 'com.crawling.mongo', 'com.crawling.spider', 'com.crawling.utils'],
-    install_requires=['bs4', 'selenium', 'pymongo', 'pymysql', 'configparser'],
     data_files=[('config.conf')]
 )
