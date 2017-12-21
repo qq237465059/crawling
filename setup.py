f836e5326e306abdca998d1e83b3dddb301dcd21
胡一 <yhu@ittx.com.cn>
2017-12-21 13:39:00 +0800

    加上版本号

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from distutils.core import setup
 
 setup(
     name='crawling',
+    version='1.0',
     description='爬虫demo',
     py_modules=['com.crawling.dao.runSpider'],
     packages=['com', 'com.crawling', 'com.crawling.config', 'com.crawling.database', 'com.crawling.domain', 'com.crawling.mongo', 'com.crawling.spider', 'com.crawling.utils'],
