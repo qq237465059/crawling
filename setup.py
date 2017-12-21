1c45f5fd31a209a4f3fab6bb1eba171ed87fe912
胡一 <yhu@ittx.com.cn>
2017-12-21 11:36:25 +0800

    打包命令

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ setup(
     name='crawling',
     version='1.0',
     description='爬虫demo',
-    py_modules=['com'],
-    packages=['com.crawling.config', 'com.crawling.database', 'com.crawling.domain', 'com.crawling.mongo', 'com.crawling.spider', 'com.crawling.utils'],
-    install_requires=['bs4', 'selenium', 'pymongo', 'pymysql', 'configparser']
+    py_modules=['com.crawling.dao.runSpider'],
+    packages=['com', 'com.crawling', 'com.crawling.config', 'com.crawling.database', 'com.crawling.domain', 'com.crawling.mongo', 'com.crawling.spider', 'com.crawling.utils'],
+    install_requires=['bs4', 'selenium', 'pymongo', 'pymysql', 'configparser'],
+    data_files=[('config.conf')]
 )
